a676351ca131408bb4f32655989d21e2dc0520dd
grzesiek <mcgregorian2012@gmail.com>
2018-02-07 19:54:13 +0100

    first release

---
+++ src/Calc.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Calc {
+
+    public static void main (String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter an integer a");
+        int a = sc.nextInt();
+        System.out.println("Enter an integer b");
+        int b= sc.nextInt();
+        
+
+    }
+}
